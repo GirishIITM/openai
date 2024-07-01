1787886564af8f07b8bb02d7c7eb3270b70e179b
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2024-07-01 19:07:59 +0530

    chore: Update dependencies and add react-router-dom to demochat project

--- demochat/src/App.tsx
+++ demochat/src/App.tsx
@@ -1,13 +1,19 @@
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 import Chat from './components/Chat'
+import FineTune from './components/FineTune'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-     <Chat/> 
-     <Toaster />
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path='/fineTune' element={<FineTune />} />
+      </Routes>
+      <Toaster />
     </>
   )
 }
