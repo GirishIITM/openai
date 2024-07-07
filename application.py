569ad2b6b71c6c42a14ea7b09625f149be60c25b
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2024-07-07 09:42:53 +0530

    chore: Update gitignore to ignore __pycache__ and add chroma directory

--- application.py
+++ application.py
@@ -7,34 +7,34 @@ from db import client
 
 uri = "mongodb+srv://gir:1@cluster0.ajxxi2q.mongodb.net/?appName=Cluster0"
 
-app = Flask(__name__)
-CORS(app)
+application = Flask(__name__)
+CORS(application)
 
 
-@app.route("/chat", methods=["POST"])
+@application.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
     response = openAiChat(data)
     return jsonify({"message": response})
 
 
-@app.route("/setFineTune", methods=["POST"])
+@application.route("/setFineTune", methods=["POST"])
 def sfineTune():
     data = request.get_json()
     response = setfineTune(data)
     return jsonify({"message": response})
 
 
-@app.route("/getFineTune", methods=["GET"])
+@application.route("/getFineTune", methods=["GET"])
 def gfineTune():
     response = getfineTune()
     return jsonify(response)
 
 
-if __name__ == '__main__':
+if __name__ == '__application__':
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
-        app.run(port=5000, debug=True)
+        application.run(host='0.0.0.0', port=8080)
     except Exception as e:
         print(e)
